6f28a4420b65a69c7e1d2f608c491413a32a7f6b
Zachary Ives <zives@hawk.iit.edu>
2024-02-23 17:51:52 -0600

    Initial Commit

---
+++ Shielding Calculations/Shielding_CalculationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shielding_CalculationsApp.swift
+//  Shielding Calculations
+//
+//  Created by Phys440Zachary on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Shielding_CalculationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
